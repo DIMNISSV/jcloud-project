f9c2f4f7b83b0d3a19b74d221e822cc07225d18e
DIMNISSV <dimnissv@yandex.kz>
2025-11-08 12:55:39 +0500

    chore(frontend): Install `tailwindcss`

--- frontend/web-app/nuxt.config.ts
+++ frontend/web-app/nuxt.config.ts
@@ -2,5 +2,18 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui']
+
+  modules: [
+    "@nuxt/ui",
+    "@pinia/nuxt",
+    '@nuxtjs/tailwindcss',
+  ],
+
+  routeRules: {
+    '/api/**': {
+      proxy: {
+        to: "http://localhost:8080/api/**", // Base path for user-service
+      }
+    },
+  }
 })
